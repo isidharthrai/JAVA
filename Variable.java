4b482c360ae84fff69c00c4a1e2994a6eb69f9cb
Blue Sky Empires <30746317+SidharthRai@users.noreply.github.com>
2017-08-18 20:08:17 +0530

    Without declaring the variables printing the unicode values.

---
+++ Variable.java
@@ -0,0 +1,9 @@
+ public class Variable {
+	public static int a;
+	public static String b;
+	public static void main(String[] args) {
+		/*Created by Sidharth Rai*/
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
